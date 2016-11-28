54dff46782d3cf34a0ed988d3d9f32316dbb620b
Grégoire Noyelle <gregorioprimo@gmail.com>
2016-11-28 16:16:02 +0100

    Ajout des modèles pour les contenus personnalisé recette la taxonomie facilite

---
+++ archive-recette.php
@@ -0,0 +1,15 @@
+<?php
+
+// règlage pour l'archive recette
+
+// Appeler le contenu du fichier filtre-recette.php
+get_template_part('filtre','recette');
+
+// Appeler le contenu du fichier contenu-recette.php
+get_template_part('contenu','recette');
+
+
+
+genesis(); 
+
+?>
---
+++ single-recette.php
@@ -0,0 +1,8 @@
+<?php
+
+// Appeler le contenu du fichier contenu-recette.php
+get_template_part('contenu','recette');
+
+genesis(); 
+
+?>
