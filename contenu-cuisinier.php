611f6afd8aa4adc08653198cbf083400a7aaa78f
Grégoire Noyelle <gregorioprimo@gmail.com>
2016-11-28 16:51:41 +0100

    Optimisation du fichier.

--- contenu-cuisinier.php
+++ contenu-cuisinier.php
@@ -8,11 +8,9 @@ add_action('genesis_entry_header', 'ap_taxo_etoile', 12 );
 function ap_taxo_etoile() {
 	// pour récupérer l'identifiant d'un post
 	global $post;
-	//echo '<pre>'
-	//var_dump ($post); exit;
-	//echo '</pre>'
+	// aff_p($post);
 
-	echo get_the_term_list( $post->ID, 'etoile', '<p class="entry-meta">', '', '</p>' );
+	echo get_the_term_list( $post->ID, 'etoile', '<p class="entry-meta">Nombre d\'étoiles&nbsp;: ', ', ', '</p>' );
 }
 
 // Affichage du contenu ACF : Fiche cuisinier
@@ -25,6 +23,10 @@ $taille_image = 'cuisinier-2';
 $img = wp_get_attachment_image($image_id, $taille_image);
 $img2 = wp_get_attachment_image( $image_id, $taille_image);
 
+// Pour afficher tous les champs ACF disponibles
+// $acf = get_fields();
+// aff_p($acf);
+
 	?>
 	<div class="biographie">
 		<h3>Biographie</h3>
@@ -35,15 +37,16 @@ $img2 = wp_get_attachment_image( $image_id, $taille_image);
 			<a href="<?php the_permalink(); ?>">
 				<?php echo $img ?>	
 			</a>
-		</div>
-			<?php } else { ?>
-				<?php echo $img ?>
-			<?php } ?>
-		
+
+			<?php } else { 
+				 	echo $img;
+			 	  } 
+			 ?>
+		</div>		
 
 		<!--  Condition pour afficher ou pas la biographie -->
 		<?php if(is_singular('cuisinier' )){ ?>
-			<div class="texte-biographie"></div>
+			<div class="texte-biographie">
 				<?php the_field('ap_biographie');?>
 			</div>
 		<?php } else { ?>
@@ -54,8 +57,8 @@ $img2 = wp_get_attachment_image( $image_id, $taille_image);
 	<div class="info-pratique">
 		<h3>Informations pratiques</h3>
 			<ul class="liste-info-pratique">
-				<li>Restaurant : <?php the_field('ap_restaurant');?></li>
-				<li>Âge :<?php the_field('ap_age');?>&nbsp;ans </li>
+				<li>Restaurant&nbsp;: <?php the_field('ap_restaurant');?></li>
+				<li>Âge&nbsp;:<?php the_field('ap_age');?>&nbsp;ans </li>
 				<li><a href="<?php the_field('ap_site_web');?>" target="_blank">Site internet</a></li>
 				<li><a href="mailto:<?php the_field('ap_email');?>">Envoyer un email</a></li>
 				<li><a href="<?php the_field('ap_cv_en_pdf');?>">Télécharger le fichier</a></li>
