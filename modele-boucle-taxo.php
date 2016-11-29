94ec62abad3a9e50b4b0f1a2d25fed3a7e8c6f9b
Grégoire Noyelle <gregorioprimo@gmail.com>
2016-11-29 11:42:30 +0100

    Mise en place de meta query sur les cuisiniers. Affichage des cuisiniers qui ont au moins 60 ans.

--- modele-boucle-taxo.php
+++ modele-boucle-taxo.php
@@ -1,7 +1,7 @@
 <?php
 // Template Name: Boucle Tax Query
 
-//* Boucle après le contenu
+//* Boucle après le contenu avec Tax Query
 add_action( 'genesis_entry_content', 'ap_boucle_tax_query', 15 );
 function ap_boucle_tax_query() { 
 $boucle_taxo = new WP_Query (
@@ -46,5 +46,46 @@ $boucle_taxo = new WP_Query (
 	<?php endif; ?>
 
 <?php } // FIN function ap_boucle_tax_query()
+
+
+
+//* Boucle après le contenu avec Meta Query
+add_action( 'genesis_entry_content', 'ap_boucle_meta_query', 20 );
+function ap_boucle_meta_query() { 
+$boucle_meta = new WP_Query (
+	array(
+		'post_type' => 'cuisinier',
+		'meta_query' => array(
+			array(
+				'key' => 'ap_age', // Champ ACF
+				'value'   => 60, // Valeur du champ
+				'type'    => 'numeric',
+				'compare' => '>=',
+			)
+		),
+		'posts_per_page' => 40
+	)
+); // fin WP_Query 
+?>	
+	<?php if( $boucle_meta->have_posts() ) :  ?>
+
+	<h3>Cuisiniers de plus de 60 ans</h3>	
+
+	<ul>
+		<?php while( $boucle_meta->have_posts() ) : $boucle_meta->the_post(); ?>
+			<li><a href="<?php the_permalink();?>"><?php the_title(); ?></a></li>
+		<?php 
+			endwhile; 
+			wp_reset_postdata();
+		?>
+	</ul>
+
+	<?php else : ?>
+
+		<p>Pas d'article avec cette requête !</p>
+
+	<?php endif; ?>
+
+<?php } // FIN function ap_boucle_meta_query()
 
 genesis();
