245b5fd2b9e58c407a9ef0af1b127bb3a9d627f1
Grégoire Noyelle <gregorioprimo@gmail.com>
2016-11-29 14:51:48 +0100

    Placement des fonctions pour l’affichage et l’appel des zones de widgets sur la page d’accueil.

--- home.php
+++ home.php
@@ -4,10 +4,45 @@
 add_action( 'genesis_meta', 'ap_contenu_page_accueil' );
 function ap_contenu_page_accueil() {
 	if ( is_active_sidebar('accueil-haut') || is_active_sidebar('accueil-bas') ) {
+
 		// Forcer la plein largeur
 		add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
 
+		//* Enlever le fil d'Ariane
+		remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+
+		//* Retirer la boucle principale
+		remove_action( 'genesis_loop', 'genesis_do_loop' );
+
+		//* Ajouter du contenu dans la zone principale avec le premier widget
+		add_action( 'genesis_loop', 'ap_accueil_haut' );
+
+			//* Ajouter du contenu dans la zone principale avec le deuxième widget
+		add_action( 'genesis_loop', 'ap_accueil_bas', 15 );
+
+
 	}
 } // FIN function ap_contenu_page_accueil()
+
+
+//* Appel des zone de widget
+
+// Accueil haut
+function ap_accueil_haut() {
+	genesis_widget_area( 'accueil-haut', array(
+		'before' => '<div class="accueil-haut widget-area">',
+		'after'  => '</div>',
+	) );
+
+}
+
+// Accueil haut
+function ap_accueil_bas() {
+	genesis_widget_area( 'accueil-bas', array(
+		'before' => '<div class="accueil-bas widget-area"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+}
 
 genesis();
