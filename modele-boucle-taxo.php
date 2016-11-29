1a52b788e476447d5583bfbbcfa7a3ba31cce08b
Grégoire Noyelle <gregorioprimo@gmail.com>
2016-11-29 10:52:58 +0100

    Mise en place complète de la tax query avec une boucle sur les cuisniers.

--- modele-boucle-taxo.php
+++ modele-boucle-taxo.php
@@ -3,8 +3,42 @@
 
 //* Boucle après le contenu
 add_action( 'genesis_entry_content', 'ap_boucle_tax_query', 15 );
-function ap_boucle_tax_query() {
- // Boucle 
-}
+function ap_boucle_tax_query() { 
+$boucle_taxo = new WP_Query (
+	array(
+		'post_type' => 'cuisinier',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'etoile',
+				'field' => 'slug',
+				'terms' => array( '1-etoile', '2-etoiles' ) 
+				)
+			),
+		'orderby' => 'title',
+		'posts_per_page' => 40
+	)
+); // fin WP_Query 
+?>	
+	<?php if( $boucle_taxo->have_posts() ) :  ?>
+
+	<div class="tous-cuisiniers">
+		<?php while( $boucle_taxo->have_posts() ) : $boucle_taxo->the_post(); ?>
+			<article class="cuisinier">
+				<h3 class="titre-cuisinier"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
+				<div class="bio-cuisinier"><?php the_field('ap_biographie'); ?></div>
+			</article>
+		<?php 
+			endwhile; 
+			wp_reset_postdata();
+		?>
+	</div>
+
+	<?php else : ?>
+
+		<p>Pas d'article avec cette requête !</p>
+
+	<?php endif; ?>
+
+<?php } // FIN function ap_boucle_tax_query()
 
 genesis();
