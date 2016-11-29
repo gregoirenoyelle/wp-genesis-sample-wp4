9b034d7ad29e1a475b71a019bd88a84aa63c2e5f
Grégoire Noyelle <gregorioprimo@gmail.com>
2016-11-29 10:13:34 +0100

    Nouveau modèle de page pour afficher une boucle personnalisée avec Tax Query NOTE
    La nouvelle boucle se trouve après le contenu principal.

---
+++ modele-boucle-taxo.php
@@ -0,0 +1,10 @@
+<?php
+// Template Name: Boucle Tax Query
+
+//* Boucle après le contenu
+add_action( 'genesis_entry_content', 'ap_boucle_tax_query', 15 );
+function ap_boucle_tax_query() {
+ // Boucle 
+}
+
+genesis();
