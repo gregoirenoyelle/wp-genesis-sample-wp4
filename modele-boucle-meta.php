32167a6d4268115dec2d510b61f80aecf4605338
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-02-16 16:54:50 +0100

    Affichage du contenu complet dans la boucle de meta

--- modele-boucle-meta.php
+++ modele-boucle-meta.php
@@ -9,6 +9,8 @@ function resto_boucle_meta() {
 // product où color=orange OU color=red et size=small cela donnera:
 $args = array(
 	'post_type'  => 'cuisinier',
+	'order'      => 'ASC',
+	'orderby'    => 'menu_order',
 	'meta_query' => array(
 		'relation'    => 'OR',
 		array(
@@ -26,21 +28,26 @@ $args = array(
 	),
 );
 $ma_boucle = new WP_Query( $args );
-?>
-<ul>
-<?php	
+
 // début loop $ma_boucle
-while ( $ma_boucle->have_posts() ) : $ma_boucle->the_post(); ?>
-	
-	<!-- les éléments de la boucle se trouvent ici -->
-	<li><?php the_title(); ?></li>
-	
-<?php endwhile;
+while ( $ma_boucle->have_posts() ) : $ma_boucle->the_post();
+// Variables
+$image_id = get_field('gob_photo');
+$taille = 'medium';
+$img = wp_get_attachment_image( $image_id, $taille );
+$lien = get_permalink();
+$titre = get_the_title();
+// aff_p($image_id); exit;	
+	// Affichage de l'image
+	printf('<div class="image"><a href="%s">%s</a></div>', $lien, $img);
+
+	// Affichage du titre
+	printf('<h3><a href="%s">%s</a></h3>', $lien, $titre );
+
+endwhile;
 wp_reset_postdata();
 // fin loop $ma_boucle	
-?>
-</ul>	
 
-<?php } // FIN function resto_boucle_meta() 
+} // FIN function resto_boucle_meta() 
 
 genesis();
