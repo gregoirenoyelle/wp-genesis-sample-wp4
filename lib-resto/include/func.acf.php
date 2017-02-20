21824801ee34c2f9b5b67149aca95c8b96ba090e
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-02-20 15:58:58 +0100

    Déclation des page d'option ACF

--- lib-resto/include/func.acf.php
+++ lib-resto/include/func.acf.php
@@ -19,3 +19,30 @@ if ( ! function_exists( 'get_field' ) ) {
 	}
 }
 
+
+if( function_exists('acf_add_options_page') ) {
+	// Page principale
+	acf_add_options_page(array(
+		'page_title'    => 'Options',
+		'menu_title'    => 'Options',
+		'menu_slug'     => 'options-generales',
+		'capability'    => 'edit_pages',
+		'icon_url'      => 'dashicons-admin-settings',
+		'position'      => '6',
+		'redirect'      => true
+	));
+  
+  // Première sous-page
+	acf_add_options_sub_page(array(
+		'page_title'    => 'Options d\'Entête',
+		'menu_title'    => 'Entête',
+		'parent_slug'   => 'options-generales',
+	));
+  // Deuxième sous-page
+	acf_add_options_sub_page(array(
+		'page_title'    => 'Options de Pied de Page',
+		'menu_title'    => 'Pied de page',
+		'parent_slug'   => 'options-generales',
+	));
+} 
+
