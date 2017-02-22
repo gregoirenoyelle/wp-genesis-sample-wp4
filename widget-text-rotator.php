5f9aa59fcc938fa9b9735ac8fc2831145fc3df16
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-02-22 15:20:16 +0100

    Ajout d'un compteur dans mon Repeater. Le dernier élément n'a pas de "*" à la fin

--- widget-text-rotator.php
+++ widget-text-rotator.php
@@ -1,15 +1,36 @@
 <?php 
 $output = '';
+$i = 1;
+// Champ Repeater principal
+$textes = get_field('wp_textr_textes', $acfw);
+// Compter les éléments du Repeater
+$compteur = count($textes);
+// aff_v($textes);
+
+// Titre
+$titre = get_field('w_textr_titre', $acfw);
+
+if ( $titre ) {
+	$output .= sprintf('<h3 class="widgettitle widget-title">%s</h3>', $titre);
+}
 
 // Contrôle si le repeater a des champs
-if( have_rows('wp_textr_textes', $acfw) ):
+if( have_rows('wp_textr_textes', $acfw) ) :
 	$output .= '<h3 class="rotate">';
 	// boucle sur les champs du repeater
 		while ( have_rows('wp_textr_textes', $acfw) ) : the_row();
 			// affichage des sous-champs
+			if ( $i < $compteur ) {
+				$separator = '*';	
+			} else {
+				$separator = '';
+			}
 			$texte = get_sub_field('w_textr_texte');
-			$output .= sprintf('%s*', $texte);
+			// $output .= $i;
+			$output .= sprintf('%s%s', $texte, $separator);
 
+		// Augmenter la valeur de $i
+		$i++;
 		endwhile;
 	$output .= '</h3>';	
 else :
