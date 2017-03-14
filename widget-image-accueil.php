7be5dcf0367cc8704525a3718437c9e56cea7998
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-03-14 08:27:49 +0100

    Suppression de la variable pour la taille d’image

--- widget-image-accueil.php
+++ widget-image-accueil.php
@@ -2,10 +2,8 @@
 
 // ID de l'image enregistré dans ACF
 $img_id = get_field('acw_image_accueil', $acfw);
-// Taille voulue
-$taille = 'full';
 // Fonction WP pour afficher la balise image
-$img = wp_get_attachment_image( $img_id, $taille );
+$img = wp_get_attachment_image( $img_id, 'full');
 // Variable pour le lien sur l'image
 $lien = get_field('acw_lien_image', $acfw);
 // Variable pour stocker tout le html à afficher
@@ -14,12 +12,12 @@ $output = '';
 
 // Affichage de l'image
 $output .= '<div class="visuel-accueil">';
-// Condition si le lien existe
-if ( $lien ) {
-    $output .= sprintf('<a href="%s">%s</a>', $lien, $img );
-} else {
-    $output .= $img;
-}
+	// Condition si le lien existe
+	if ( $lien ) {
+	    $output .= sprintf('<a href="%s">%s</a>', $lien, $img );
+	} else {
+	    $output .= $img;
+	}
 $output .= '</div>';
 // Affichage de tout le HTML contenu dans la variable $output
 echo $output;
