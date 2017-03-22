fc4e4717a4e60a106adb60501105f71fb1235576
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-03-22 08:43:57 +0100

    Suppression de la despcription de la catégorie

--- category.php
+++ category.php
@@ -4,5 +4,7 @@
 // Maquette Pleine largeur
 add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
 
+remove_action( 'genesis_before_loop', 'genesis_do_taxonomy_title_description', 15 );
+
 
 genesis();
